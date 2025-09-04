b811ba629df8ca2a5c2deaaa77843970980c43d3
Edinei Lucas Silva Souza <edineilucas001@gmail.com>
2025-09-04 08:49:49 -0300

    Feito a função inicializa lista

--- Lista.c
+++ Lista.c
@@ -1,3 +1,13 @@
-#include <lista.h>
+#include "lista.h"
 #include <stdio.h>
 #include <stdlib.h>
+
+/* Inicializa a lista como lista vazia. */
+void inicializa_lista(Lista * ap_lista){
+    *ap_lista = NULL;
+}
+
+
+int main  (){
+    return 0;
+}
